8f49416d68ef251f98eb9fd77c55f9448ac76ee0
Valter Valik <valtervaralik@gmail.com>
2023-12-15 00:22:23 -0500

    Change userId parameter type to string in RefreshTokenIdsStorage class

--- src/auth/authentication/refresh-token-ids.storage/refresh-token-ids.storage.ts
+++ src/auth/authentication/refresh-token-ids.storage/refresh-token-ids.storage.ts
@@ -23,11 +23,11 @@ export class RefreshTokenIdsStorage
     return this.redisClient.quit();
   }
 
-  async insert(userId: number, tokenId: string): Promise<void> {
+  async insert(userId: string, tokenId: string): Promise<void> {
     await this.redisClient.set(this.getKey(userId), tokenId);
   }
 
-  async validate(userId: number, tokenId: string): Promise<boolean> {
+  async validate(userId: string, tokenId: string): Promise<boolean> {
     const storedTokenId = await this.redisClient.get(this.getKey(userId));
 
     if (storedTokenId !== tokenId) {
@@ -37,11 +37,11 @@ export class RefreshTokenIdsStorage
     return storedTokenId === tokenId;
   }
 
-  async invalidate(userId: number): Promise<void> {
+  async invalidate(userId: string): Promise<void> {
     await this.redisClient.del(this.getKey(userId));
   }
 
-  private getKey(userId: number): string {
+  private getKey(userId: string): string {
     return `user-${userId}`;
   }
 }
