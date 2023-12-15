b8f7a2b0b6b1b56469b28df496b1b42eac235d3f
Valter Valik <valtervaralik@gmail.com>
2023-12-15 00:21:55 -0500

    Update imports in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common';
 import { HashingService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/users/entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import jwtConfig from './config/jwt.config';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
-import { Role } from 'src/roles/entities/role.entity';
-import { Permission } from 'src/permissions/entities/permission.entity';
+import { Role, RoleSchema } from 'src/roles/schemas/role.schema';
+import {
+  Permission,
+  PermissionSchema,
+} from 'src/permissions/schemas/permission.schema';
 import { PermissionsService } from 'src/permissions/permissions.service';
-import { ApiKey } from 'src/users/api-keys/entities/api-key.entity/api-key.entity';
+import {
+  ApiKey,
+  ApiKeySchema,
+} from 'src/users/api-keys/schemas/api-key.schema';
 import { AuthGuard } from './authentication/guards/auth.guard';
 import { RolesGuard } from './authorization/guards/roles.guard';
 import { PermissionsGuard } from './authorization/guards/permissions.guard';
@@ -26,10 +30,17 @@ import { GoogleAuthService } from './authentication/social/google-auth.service';
 import { GoogleAuthController } from './authentication/social/google-auth.controller';
 import { AuthenticationService } from './authentication/authentication.service';
 import { AuthenticationController } from './authentication/authentication.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/users/schemas/user.schema';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, ApiKey, Role, Permission]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Role.name, schema: RoleSchema },
+      { name: Permission.name, schema: PermissionSchema },
+      { name: ApiKey.name, schema: ApiKeySchema },
+    ]),
     JwtModule.registerAsync(jwtConfig.asProvider()),
     ConfigModule.forFeature(jwtConfig),
   ],
