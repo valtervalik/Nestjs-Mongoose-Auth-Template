372343ccc6e5d6c837541cb32585d09eb6f42d40
Valter Valik <valtervaralik@gmail.com>
2024-01-01 20:59:23 -0500

    Add user roles enum and update users controller and service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,24 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { ApiKey, ApiKeySchema } from './api-keys/schemas/api-key.schema';
-import { Role, RoleSchema } from 'src/roles/schemas/role.schema';
-import {
-  Permission,
-  PermissionSchema,
-} from 'src/permissions/schemas/permission.schema';
-import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from './schemas/user.schema';
+import { UsersService } from './users.service';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([
-      { name: User.name, schema: UserSchema },
-      { name: ApiKey.name, schema: ApiKeySchema },
-      { name: Role.name, schema: RoleSchema },
-      { name: Permission.name, schema: PermissionSchema },
-    ]),
-  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { BaseService } from 'src/base/base.service';
+import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
-export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
-
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
-}
+export class UsersService extends BaseService<UserDocument>(User.name) {}
