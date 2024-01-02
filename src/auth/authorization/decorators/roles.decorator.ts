d5a23ff1b259bab409747ca2492d207dda37f369
Valter Valik <valtervaralik@gmail.com>
2024-01-01 20:56:57 -0500

    Update import path for UserRoles enum

--- src/auth/authorization/decorators/roles.decorator.ts
+++ src/auth/authorization/decorators/roles.decorator.ts
@@ -1,5 +1,5 @@
 import { SetMetadata } from '@nestjs/common';
 import { ROLES_KEY } from 'src/auth/auth.constants';
-import { UserRoles } from 'src/roles/enums/user-roles.enum';
+import { UserRoles } from 'src/users/enums/user-roles.enum';
 
 export const Roles = (...roles: UserRoles[]) => SetMetadata(ROLES_KEY, roles);
