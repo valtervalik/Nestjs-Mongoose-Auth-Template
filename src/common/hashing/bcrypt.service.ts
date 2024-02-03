de97bdd108e88830fbdcd1c1bb9218c918590f0f
Valter Valik <valtervaralik@gmail.com>
2024-02-03 18:09:49 -0500

    style: :art: Reorder import statements in BcryptService

--- src/common/hashing/bcrypt.service.ts
+++ src/common/hashing/bcrypt.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { HashingService } from './hashing.service';
 import { compare, genSalt, hash } from 'bcrypt';
+import { HashingService } from './hashing.service';
 
 @Injectable()
 export class BcryptService implements HashingService {
