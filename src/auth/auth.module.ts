a83a629634fe67ff12b758b2bc9384c3ac06de52
Valter Valik <valtervaralik@gmail.com>
2024-02-18 20:35:47 -0500

    feat: :sparkles: Add PassportModule and update authentication strategies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { EncryptingModule } from 'src/common/encrypting/encrypting.module';
 import { HashingModule } from 'src/common/hashing/hashing.module';
 import { PermissionsModule } from 'src/permissions/permissions.module';
@@ -9,9 +10,11 @@ import { RolesModule } from 'src/roles/roles.module';
 import { UsersModule } from 'src/users/users.module';
 import { AuthenticationController } from './authentication/authentication.controller';
 import { AuthenticationService } from './authentication/authentication.service';
-import { AccessTokenGuard } from './authentication/guards/access-token.guard';
 import { AuthGuard } from './authentication/guards/auth.guard';
+import { JwtAuthGuard } from './authentication/guards/jwt-auth.guard';
 import { RefreshTokenIdsStorage } from './authentication/refresh-token-ids.storage/refresh-token-ids.storage';
+import { JwtStrategy } from './authentication/strategies/jwt.strategy';
+import { LocalStrategy } from './authentication/strategies/local.strategy';
 import { TwoFactorAuthService } from './authentication/two-factor-auth/two-factor-auth.service';
 import { PermissionsGuard } from './authorization/guards/permissions.guard';
 import { RolesGuard } from './authorization/guards/roles.guard';
@@ -26,15 +29,18 @@ import jwtConfig from './config/jwt.config';
     ConfigModule.forFeature(jwtConfig),
     HashingModule,
     EncryptingModule,
+    PassportModule,
   ],
   providers: [
     { provide: APP_GUARD, useClass: AuthGuard },
     { provide: APP_GUARD, useClass: RolesGuard },
     { provide: APP_GUARD, useClass: PermissionsGuard },
-    AccessTokenGuard,
+    JwtAuthGuard,
     RefreshTokenIdsStorage,
     AuthenticationService,
     TwoFactorAuthService,
+    LocalStrategy,
+    JwtStrategy,
   ],
   controllers: [AuthenticationController],
   exports: [AuthenticationService],
