8e1aa617c6219ab0776a7fc2d0d1b1c9b26dd46c
Valter Valik <valtervaralik@gmail.com>
2024-06-01 22:28:03 -0400

    refactor: :recycle: refactor: Update Role schema to extend BaseSchema

--- src/roles/schemas/role.schema.ts
+++ src/roles/schemas/role.schema.ts
@@ -1,10 +1,11 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document, HydratedDocument } from 'mongoose';
+import { HydratedDocument } from 'mongoose';
+import { BaseSchema } from '../../base/base.schema';
 
 export type RoleDocument = HydratedDocument<Role>;
 
 @Schema()
-export class Role extends Document {
+export class Role extends BaseSchema {
   @Prop({ type: String, required: true })
   name: string;
 }
