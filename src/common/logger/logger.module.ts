d21efc1eb8586bdff63b961bd052d3ceabd1c13b
Valter Valik Alfonso Rivero <valtervaralik@gmail.com>
2025-07-06 06:39:00 -0400

    feat: :sparkles: add LoggerModule with Pino for structured logging

---
+++ src/common/logger/logger.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { LoggerModule as PinoLoggerModule } from 'nestjs-pino';
+
+@Module({
+  imports: [
+    PinoLoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+        transport:
+          process.env.NODE_ENV !== 'production'
+            ? {
+                target: 'pino-pretty',
+                options: {
+                  colorize: true,
+                  translateTime: 'SYS:standard',
+                },
+              }
+            : undefined,
+      },
+    }),
+  ],
+})
+export class LoggerModule {}
